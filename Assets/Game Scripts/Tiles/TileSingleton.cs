d5df104f775c5813084ecc568bf03e30f35685d8
Peter Slattery <slattery.peter.j@gmail.com>
2016-01-30 04:00:43 -0500

    Created Tile Scripts

---
+++ Assets/Game Scripts/Tiles/TileSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileSingleton : MonoBehaviour {
+
+    public List<GameObject> m_prefabs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void InstantiatePrefabOfType()
+    {
+        foreach(GameObject obj in m_prefabs)
+        {
+
+        }
+    }
+}
