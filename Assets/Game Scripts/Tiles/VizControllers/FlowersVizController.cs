ea7ce523c419a07fe4ef556d3964161c32f7153e
Peter Slattery <slattery.peter.j@gmail.com>
2016-01-31 17:19:05 -0500

    Made Visual Updates

--- Assets/Game Scripts/Tiles/VizControllers/FlowersVizController.cs
+++ Assets/Game Scripts/Tiles/VizControllers/FlowersVizController.cs
@@ -58,48 +58,10 @@ public class FlowersVizController : TileVizController {
 	}
 
 	void ShowFlowersForGrowthLevel (float growth, int flowerRatio){
-		if (growth > 6.6) {
-			for (int c = 0; c < m_verticalElements.transform.childCount; c++) {
-				GameObject child = m_verticalElements.transform.GetChild (c).gameObject;
-				if (child.activeSelf) {
-					Destroy (child);
-				}
-			}
-			Instantiate (flowerBush, m_verticalElements.transform.position, Quaternion.identity);
-		} else {
-			int numFlowers = (int)(growth * flowerRatio);
-			numFlowers -= m_verticalElements.transform.childCount - numBuds;
-
-			if (numFlowers >= 0) {
-				for (int i = 0; i < numFlowers; i++) {
-				 
-					GameObject toInst;
-
-					float chanceFlower = Random.Range (0.0f, 10.0f);
-
-					if (chanceFlower + growth > 8.0f) {
-						toInst = flowers [0];//Random.Range (0, lastFlower)];
-					} else {
-						toInst = flowerBuds [0];//Random.Range (0, lastBud)];
-					}
-
-					Vector2 offset = Random.insideUnitCircle;
-
-			/*		GameObject flower = Instantiate (toInst,
-						                   m_verticalElements.transform.position + (new Vector3 (offset.x, 0, offset.y) * (1.73f / 2)),
-						                   Quaternion.identity) as GameObject;		
-					flower.transform.SetParent (m_verticalElements.transform);
-					flower.SetActive (true);
-			*/
-				}
-			} else {
-				for (int i = 0; i > numFlowers; i--) {
-					GameObject child = m_verticalElements.transform.GetChild (Random.Range (0, m_verticalElements.transform.childCount - 1)).gameObject;
-					if (child.activeInHierarchy) {
-						Destroy (child);
-					}
-				}
-			}
+		if (growth > 7.0f) {
+			GameObject newBush = Instantiate (flowerBush, m_verticalElements.transform.position, flowerBush.transform.rotation) as GameObject;
+			newBush.transform.SetParent (m_verticalElements.transform);
+			newBush.SetActive (true);
 		}
 	}
 
--- Assets/Game Scripts/Tiles/VizControllers/StaticVizController.cs
+++ Assets/Game Scripts/Tiles/VizControllers/StaticVizController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class StaticVizController : TileVizController {
 
+	GameObject[] verticals;
+
 	// Use this for initialization
 	public override void Start () {
 		base.Start ();
@@ -13,6 +15,19 @@ public class StaticVizController : TileVizController {
 	public override void InitializeViz() {
 		if (!m_initialized) {
 			base.InitializeViz ();
+
+			verticals = new GameObject[m_verticalElements.transform.childCount];
+			for (int i=0; i<m_verticalElements.transform.childCount; i++){
+				verticals [i] = m_verticalElements.transform.GetChild (i).gameObject;
+			}
+
+			int numRocks = Random.Range (0, verticals.Length - 1);
+			for (int i = 0; i < numRocks; i++) {
+				Vector2 circle = Random.insideUnitCircle;
+				Vector3 XYcircle = new Vector3 (circle.x, 0, circle.y);
+				GameObject newRock = Instantiate (verticals [Random.Range (0, verticals.Length - 1)], m_verticalElements.transform.position + XYcircle, Quaternion.identity) as GameObject;
+				newRock.transform.parent = m_verticalElements.transform;
+			}
 		}
 	}
 	
